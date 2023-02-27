65beed7fffcd7db6be7a2ba4695c05d03c89da77
Alex Wong <wongalexjx@gmail.com>
2023-02-26 16:20:18 -0800

    adding server routes / api

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,6 +9,12 @@ app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the movie posters api");
+  }
+);
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello Worlc" });
+    res.json({ message: "Hello World" });
   });
+
