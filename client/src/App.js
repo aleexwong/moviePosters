49c748e1ad2bf95b472bc7806a02ed07a9844006
Alex Wong <wongalexjx@gmail.com>
2023-03-12 16:57:55 -0700

    added navbar and some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import NavBar from './components/navBar';
 import MoviePosterCard from './components/moviePosterCard';
+
+
 function App() {
     const [movie, setMovie] = useState("");
+    const [search, setSearch] = useState("");
 
     return (
         <div className="App">
-        <h1>Movies</h1>
-        <MoviePosterCard />
+            <NavBar />  
+            <h1>Results</h1>
+            <input type="text" onChange={(e) => setSearch(e.target.value)} />
+            <button onClick={() => console.log(search)}>Search</button>
+            <br />
+            <MoviePosterCard />
         </div>
     );
 
