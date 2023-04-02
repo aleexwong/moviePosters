595349b1b5f54cb25758164f36f1ece123636dbb
Alex Wong <wongalexjx@gmail.com>
2023-04-02 13:48:42 -0700

    pagination working

--- client/src/components/moviePosterCard.js
+++ client/src/components/moviePosterCard.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function MoviePosterCard(props) {
   const [movie, setMovie] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const result = [];
+  const pageCount = useRef(1);
 
   const handleSearch = () => {
-    fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=a35d47f2`)
+    fetch(
+      `https://www.omdbapi.com/?s=${searchQuery}&page=${pageCount.current}&apikey=a35d47f2`
+    )
       .then((res) => res.json())
       .then((data) => {
         setSearchResults(data.Search);
-        console.log(data.Search);
+        console.log(pageCount);
       });
   };
 
@@ -32,12 +34,36 @@ function MoviePosterCard(props) {
         {searchResults &&
           searchResults.map((result) => (
             <div key={result.imdbID}>
-              <li>{result.Title}</li>
+              <div>{result.Title}</div>
               <img src={result.Poster} alt={result.title} />
             </div>
           ))}
       </ul>
+      <div className="page-buttons">
+        <button
+          onClick={() => {
+            if (pageCount <= 1) {
+              pageCount = 1;
+              handleSearch();
+            } else {
+              pageCount.current--;
+              handleSearch();
+            }
+          }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => {
+            pageCount.current++;
+            handleSearch();
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
+
 export default MoviePosterCard;
