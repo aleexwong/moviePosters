cceb71c6ab9b3e4403d1dd2905127c0410122e3d
Alex Wong <wongalexjx@gmail.com>
2023-10-09 01:23:27 -0700

    found better way of error handling

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -11,17 +11,8 @@ function Pagination(props) {
 
   return (
     <div className="page-buttons">
-      {/* <button onClick={handlePrevPage}>Prev</button> */}
-      {numberOfReturnSearchs >= 10 && currentPageNumber > 1 && (
-        <button onClick={handlePrevPage}>Prev</button>
-      )}
-      {numberOfReturnSearchs < 10 && currentPageNumber > 1 && (
-        <button onClick={handlePrevPage}>Prev</button>
-        /* Handling the case of the very end of the search results*/
-      )}
-      {/* {currentPageNumber > 1 && <button onClick={handlePrevPage}>Prev</button>} */}
+      {currentPageNumber > 1 && <button onClick={handlePrevPage}>Prev</button>}
       <button>{currentPageNumber}</button>
-      {/* <button onClick={handleNextPage}>Next</button> */}
       {numberOfReturnSearchs >= 10 && (
         <button onClick={handleNextPage}>Next</button>
       )}
